c3720a3f82667bc2cfdb17e78d4a05463a7ae950
enchant3dmango <andresitorusz.19@gmail.com>
2025-01-29 02:29:08 +0700

    chore: tidy up

--- foundry-fundamentals/foundry-fund-me/script/HelperConfig.s.sol
+++ foundry-fundamentals/foundry-fund-me/script/HelperConfig.s.sol
@@ -8,37 +8,21 @@ abstract contract CodeConstants {
     uint8 public constant DECIMALS = 8;
     int256 public constant INITIAL_PRICE = 2000e8;
 
-    /*//////////////////////////////////////////////////////////////
-                               CHAIN IDS
-    //////////////////////////////////////////////////////////////*/
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
 
 contract HelperConfig is CodeConstants, Script {
-    /*//////////////////////////////////////////////////////////////
-                                 ERRORS
-    //////////////////////////////////////////////////////////////*/
     error HelperConfig__InvalidChainId();
-
-    /*//////////////////////////////////////////////////////////////
-                                 TYPES
-    //////////////////////////////////////////////////////////////*/
     struct NetworkConfig {
         address priceFeed;
     }
 
-    /*//////////////////////////////////////////////////////////////
-                            STATE VARIABLES
-    //////////////////////////////////////////////////////////////*/
     // Local network state variables
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
 
-    /*//////////////////////////////////////////////////////////////
-                               FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
         networkConfigs[ZKSYNC_SEPOLIA_CHAIN_ID] = getZkSyncSepoliaConfig();
@@ -57,13 +41,11 @@ contract HelperConfig is CodeConstants, Script {
         }
     }
 
-    /*//////////////////////////////////////////////////////////////
-                                CONFIGS
-    //////////////////////////////////////////////////////////////*/
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return
             NetworkConfig({
-                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
+                // ETH / USD
+                priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
             });
     }
 
@@ -74,13 +56,11 @@ contract HelperConfig is CodeConstants, Script {
     {
         return
             NetworkConfig({
-                priceFeed: 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF // ETH / USD
+                // ETH / USD
+                priceFeed: 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF
             });
     }
 
-    /*//////////////////////////////////////////////////////////////
-                              LOCAL CONFIG
-    //////////////////////////////////////////////////////////////*/
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         // Check to see if we set an active network config
         if (localNetworkConfig.priceFeed != address(0)) {
@@ -88,7 +68,7 @@ contract HelperConfig is CodeConstants, Script {
         }
 
         console2.log(unicode"⚠️ You have deployed a mock contract!");
-        console2.log("Make sure this was intentional");
+        console2.log("Make sure this was intentional.");
         vm.startBroadcast();
         MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
             DECIMALS,
